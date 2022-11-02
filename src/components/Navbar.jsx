301c3f75c03a0e447f9182f00ba0cf1ddaa5f4df
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-01 21:22:32 -0600

    Navbar - elementos cambiados de a a NavLink y href a to

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Taller de recuperación</a>
+    <NavLink className="navbar-brand" to="/">Taller de recuperación</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Inicio</a>
+          <NavLink className="nav-link active" aria-current="page" to="/">Inicio</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">CRUD</a>
+          <NavLink className="nav-link" to="/crud">CRUD</NavLink>
         </li>
       </ul>
       <form className="d-flex" role="search">
