21a330f04e8a243cde98f1bda9713e3bb9c751a2
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-01 20:40:29 -0600

    Importación de Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import MiComponente from "./components/MiComponente";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+    <Navbar/>
     <h1>Proyecto de REACT limpio</h1>
+
     <MiComponente/>
+
     </>
   );
 }
