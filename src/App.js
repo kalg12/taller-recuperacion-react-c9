9cbff0fdf41506e1a339c84590367070ff16aec5
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-11-01 20:20:40 -0600

    Nuevo componente + Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import MiComponente from "./components/MiComponente";
+
 function App() {
   return (
+    <>
     <h1>Proyecto de REACT limpio</h1>
+    <MiComponente/>
+    </>
   );
 }
 
---
+++ src/components/MiComponente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MiComponente = () => {
+  return (
+    <>
+    <h3>Hola, soy un componente</h3>
+    <h4>Taller de recuperación</h4>
+    </>
+  )
+}
+
+export default MiComponente
